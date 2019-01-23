9c5a6777b8224d35f9d706bf0b6cd0fff55386e4
auvmacintosh <auvmacintosh@qq.com>
2019-01-23 21:38:46 +0800

    start masonry layout

--- src/App.js
+++ src/App.js
@@ -13,23 +13,23 @@ class App extends React.Component {
     // }
 
     render() {
-        const PostWithData =
-            withHalJsonResources('http://localhost:3000/staticapi/post')(Post);
-        const CommentListWithData =
-            withHalJsonResources('http://localhost:3000/staticapi/comments')(CommentBox);
-        return (
-            <div className={style.container}>
-                <div className={style.brace}/>
-                <PostWithData />
-                <CommentListWithData />
-            </div>
-        )
+        // const PostWithData =
+        //     withHalJsonResources('http://localhost:3000/staticapi/post')(Post);
+        // const CommentListWithData =
+        //     withHalJsonResources('http://localhost:3000/staticapi/comments')(CommentBox);
+        // return (
+        //     <div className={style.container}>
+        //         <div className={style.brace}/>
+        //         <PostWithData />
+        //         <CommentListWithData />
+        //     </div>
+        // )
 
         // return <Login></Login>
 
-        // return (
-        //     <RandomHeightBlock id={1} width={'20rem'}/>
-        // )
+        return (
+            <RandomHeightBlock id={1} width={'20rem'}/>
+        )
     }
 }
 
--- src/withMasonryLayout.js
+++ src/withMasonryLayout.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const withMasonryLayout =  MyComponent => {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            this.columnNo = 3;
+            // this.props.xs 保存各个Item的内容和高度，高度是set callback传进来的
+            this.layoutLength = new Array(this.columnNo); // 一维数组，每列的长度。
+            this.layoutLength.map(()=>0);
+            this.state = {layout: new Array(this.columnNo)}; // 二维数组，存的是每列包含哪些xs
+            this.state.layout.map(()=>[]);
+        }
 
+        
+    }
 }
 
 export default withMasonryLayout;
