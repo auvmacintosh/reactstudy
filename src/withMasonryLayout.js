932da088a5924e37e9849fc10c7824d30da5c537
auvmacintosh <auvmacintosh@qq.com>
2019-01-30 21:28:02 +0800

    继续优化了一下代码

--- src/withMasonryLayout.js
+++ src/withMasonryLayout.js
@@ -2,48 +2,22 @@ import React from 'react';
 import PropTypes from "prop-types";
 import {compose, map} from "ramda";
 
-const withSetHeightOnDidMount = setHeight => Component => {
-    class ItemWithComponentAndSetHeight extends React.Component {
+const withSetHeightOnDidMount = setHeight => {
+    return class ItemWithComponentAndSetHeight extends React.Component {
         componentDidMount() {
             const height = this.divElement.clientHeight;
-            console.log(height)
             setHeight(height);
         }
 
         render() {
-            console.log('fuck')
             return (
                 <div ref={(divElement) => this.divElement = divElement}>
-                    <Component item={this.props.item}/>
+                    {this.props.children}
                 </div>
             )
         }
     }
-
-    ItemWithComponentAndSetHeight.propTypes = {
-        item: PropTypes.object,
-    };
-
-    return ItemWithComponentAndSetHeight;
 }
-//
-// const ItemOld = (Component) => (setHeight) => (item) => {
-//     return <Component setHeight={setHeight}
-//                       item={item}
-//     />
-// }
-
-const Column = items => (
-    <div style={{display: 'flex', flexDirection: 'column', width: '20rem',}}>
-        {items}
-    </div>
-)
-
-const Table = columns => (
-    <div style={{display: 'flex', flexDirection: 'row',}}>
-        {columns}
-    </div>
-)
 
 const withMasonryLayout = Component => {
     class ComponentWithMasonryLayout extends React.Component {
@@ -55,14 +29,13 @@ const withMasonryLayout = Component => {
 
             this.layouts = {};
             this.layouts[this.columnNo] = {
-                columns: [...Array(this.columnNo)].map(() => []), // 二维数组，存的是每列包含的那部分xs的indexInAllXs
+                itemIndexMatrix: [...Array(this.columnNo)].map(() => []), // 二维数组，存的是每列包含的那部分xs的indexInAllXs
                 columnHeights: Array(this.columnNo).fill(0), // 一维数组，每列的高度。
                 lastItemIndex: -1,
             };
 
-            this.Item = withSetHeightOnDidMount(this.setHeight)(Component);
             this.state = {
-                columns: this.layouts[this.columnNo].columns
+                itemIndexMatrix: this.layouts[this.columnNo].itemIndexMatrix
             };
         }
 
@@ -76,14 +49,15 @@ const withMasonryLayout = Component => {
         }
 
         set item(item) {
-            this.layout.columns[this.shortestColumnIndex].push(this.items.push(item) - 1);
+            this.layout.itemIndexMatrix[this.shortestColumnIndex].push(this.items.push(item) - 1);
         };
 
-        setHeight = (height) => {
-            // console.log(this.layout)
+        setHeight = height => {
             this.itemHeights.push(height);
             this.layout.columnHeights[this.shortestColumnIndex] += height;
         };
+
+        getItem = i => this.items[i];
 
         componentDidMount() {
             this.props.getXs().then(response => {
@@ -106,17 +80,37 @@ const withMasonryLayout = Component => {
         // handleScrolledToBottom = () => {
         // };
 
+        Table = columns => (
+            <div style={{display: 'flex', flexDirection: 'row',}}>
+                {columns}
+            </div>
+        );
+
+        Column = items => (
+            <div style={{display: 'flex', flexDirection: 'column', width: '20rem',}}>
+                {items}
+            </div>
+        );
+
+        ComponentWithSetHeightOnDidMount = withSetHeightOnDidMount(this.setHeight);
+        Item = item => (
+            <this.ComponentWithSetHeightOnDidMount>
+                <Component item={item}/>
+            </this.ComponentWithSetHeightOnDidMount>
+        );
 
         render() {
             return (
                 <>
                     {
-                        compose(Table,
-                            map(Column),
-                            map(map(item => <this.Item item={item}/>)),
-                            // map(map(ItemOld(Component)(this.setHeight))),
-                            map(map(x => this.items[x]))
-                        )(this.state.columns)
+                        // map(f)就是一层递归，也就是对Array里的每个元素使用f函数
+                        // map(map(f))就是两层递归
+                        compose( // compose的作用就是把下边的函数串联起来
+                            this.Table, // 打包一个table
+                            map(this.Column), // 打包n个列
+                            map(map(this.Item)), // 打包n*m个Item
+                            map(map(this.getItem)) // 根据index查出对应的对象
+                        )(this.state.itemIndexMatrix) // 输入是一个n*m维的矩阵，每个元素都是index
                     }
                 </>
             )
