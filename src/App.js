5129bc88f7f8995e37342d162561849db7aea8aa
auvmacintosh <auvmacintosh@qq.com>
2019-03-13 17:34:39 +0800

    开始写login模块，xhr还没写完

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 let log = console.log.bind(console);
 
+const UserContext = React.createContext({name: null});
+const LocaleContext = React.createContext({language: 'en_US'});
+
 const App = () => {
+    const [user, setUser] = React.useState({name: null});
+    const [locale, setLocale] = React.useState({language: 'en_US'});
+
     return (
         <Router>
-            <>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/login/:id'>Login</Link></li>
-                </ul>
-                <div>
-                    <Route exact={true} path='/' component={Home}/>
-                    <Route path='/login/:id' component={Login}/>
-                </div>
-            </>
+            <UserContext.Provider value={user}>
+                <LocaleContext.Provider value={locale}>
+                    {user.name === null ? <Login/> : <Home/>}
+                </LocaleContext.Provider>
+            </UserContext.Provider>
         </Router>
     )
 };
@@ -25,8 +26,49 @@ const Home = () => {
     return <div>home</div>
 };
 
-const Login = (props) => {
-    return <div>login</div>
+const Login = () => {
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const handler = (e) => {
+        e.preventDefault();
+        switch (e.target.name) {
+            case 'username' :
+                setUsername(e.target.value);
+                break;
+            case 'password' :
+                setPassword(e.target.value);
+                break;
+            case 'submit' :
+                const xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = () => {};
+                xhr.open('POST', 'http://localhost:8080/login', true)
+                xhr.send();
+                break;
+        }
+    };
+
+    return (
+        <form onSubmit={handler}>
+            <input
+                type='text'
+                name='username'
+                value={username}
+                onChange={handler}
+                placeholder='Name'
+            />
+            <input
+                type='password'
+                name='password'
+                value={password}
+                onChange={handler}
+                placeholder='Password'
+            />
+            <button
+                type="submit"
+                name='submit'
+            >Login</button>
+        </form>
+    )
 };
 
 export default App;
