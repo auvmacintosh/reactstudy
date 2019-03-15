46c749e530c97db03702e76649e58c46984c9994
auvmacintosh <auvmacintosh@qq.com>
2019-03-15 16:32:45 +0800

    finish hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import 'normalize.css';
 
-const App = () => {
-    const [c1, setC1] = React.useState(1);
-    const [c2, setC2] = React.useState(1);
-
-    return (
-        <>
-            <div>{c1}</div>
-            <div>{c2}</div>
-            <B c1={c1} setC1={setC1}>C1</B>
-            <B c1={c2} setC1={setC2}>C2</B>
-        </>
-    )
-};
-
-const B = React.memo(({c1, setC1, children}) => {
+const useDocumentTitle = count => {
     React.useEffect(() => {
-        console.log(children + ' effect happened'); // initial render, later render(后)的时候执行
-        return () => {
-            console.log(children + ' cleanup happened')
-        }; // later render(先)，unmount的时候执行
-    });
-    return <button onClick={() => setC1(c1 + 1)}>{children}</button>
-});
+        document.title = count;
+    }, [count]);
+}
+const App = () => {
+    const [count, setCount] = React.useState(1);
+    useDocumentTitle(count);
+    return <button onClick={() => setCount(count + 1)}>+1</button>
+};
 
 export default App;
