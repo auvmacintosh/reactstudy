108c37d9ef47188f3de7a5f023b533cd8872dffd
auvmacintosh <auvmacintosh@qq.com>
2019-04-23 21:34:14 +0800

    继续

--- src/CellArrangement.js
+++ src/CellArrangement.js
@@ -1,10 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext, useRef} from 'react';
 import PropTypes from "prop-types";
 import {ContextFs, ContextWiw} from "./GlobalState";
-import * as R from "ramda";
 import MasonryLayout from "./MasonryLayout";
 
-const idxMp = R.addIndex(R.map);
 const MIN_COLUMN_NO = 2; // 最少这么多列
 const COLUMN_WIDTH = 20; // rem
 const HALF_GAP = 0.8; // rem
@@ -14,7 +12,7 @@ class ColumnNoObj {
     constructor(columnNo) {
         this.itemIndexMatrix = [...Array(columnNo)].map(() => []); // 二维数组，存的是每列包含的那部分xs的indexInAllXs
         this.columnHeights = Array(columnNo).fill(0); // 一维数组，每列的高度。
-        this.lastCellItemIndex = -1;
+        this.lastCellsItemIndex = -1;
     }
 }
 
@@ -40,7 +38,6 @@ class Layouts {
     }
 }
 
-let layouts = new Layouts();
 // let layouts = {
 //     COLUMN_WIDTH: 20, // rem
 //     MIN_COLUMN_NO: 2, // 最少这么多列
@@ -58,46 +55,53 @@ let layouts = new Layouts();
 //
 //     '18px': {},
 // };
+let layouts = new Layouts();
+
+const calculateColumnNo = (wiw) => {
+    let htmlFontSize = parseInt(getComputedStyle(document.documentElement).fontSize);
+    let completeColumnNo = Math.floor((wiw - 2 * HALF_GAP * htmlFontSize)
+        / (COLUMN_WIDTH * htmlFontSize));
+    // 最小也得是2列，不能返回0列
+    return Math.max(MIN_COLUMN_NO, completeColumnNo);
+};
+
+const getFoCno = (fs, wiw) => {
+    layouts.getFo(fs).getCno(calculateColumnNo(wiw));
+};
+
+const getShortestColumnIndex = (fs, wiw) => {
+    return getFoCno(fs, wiw).columnHeights.indexOf(
+        Math.min(...getFoCno(fs, wiw).columnHeights));
+};
+
+const setHeight = (fs, wiw, height) => {
+    layouts.getFo(fs).cellHeights.push(height);
+    getFoCno(fs, wiw).columnHeights[getShortestColumnIndex(fs, wiw)] += height;
+};
 
 const CellArrangement = ({items}) => {
-    const fs = React.useContext(ContextFs); // Factor string, i.e Font size
-    const wiw = React.useContext(ContextWiw); // Window inner width
+    const fs = useContext(ContextFs); // Factor string, i.e Font size
+    const wiw = useContext(ContextWiw); // Window inner width
 
-    const getColumnNo = () => {
-        let htmlFontSize = parseInt(getComputedStyle(document.documentElement).fontSize);
-        let completeColumnNo = Math.floor((wiw - 2 * HALF_GAP * htmlFontSize)
-            / (COLUMN_WIDTH * htmlFontSize));
-        // 最小也得是2列，不能返回0列
-        return Math.max(MIN_COLUMN_NO, completeColumnNo);
-    };
+    const getItem = useRef(i => {
+        return items[i];
+    });
 
-    const getLayout = () => {
-        layouts.getFo(fs).getCno(getColumnNo(wiw));
-    };
+    const [matrix, setMatrix] = useState();
 
-    const getShortestColumnIndex = () => {
-        return getLayout(fs, wiw).columnHeights.indexOf(
-            Math.min(...getLayout(fs, wiw).columnHeights));
-    };
-
-    const setHeight = (height) => {
-        layouts.getFo(fs).cellHeights.push(height);
-        getLayout().columnHeights[getShortestColumnIndex()] += height;
-    };
-
-    const setCell = item => {
-        this.layout.lastCellItemIndex = this.items.push(item) - 1;
-        // itemIndexMatrix能改，我觉得是因为是shadow freeze的，这个得再试试。另外react应该不推荐这个写法。
-        this.layout.itemIndexMatrix[this.shortestColumnIndex].push(this.layout.lastCellItemIndex);
-        this.setState(prevState => prevState);
-    };
-
-    const getCell = (i) => {
-        return this.items[i];
-    };
+    if (getFoCno(fs, wiw).lastCellsItemIndex + 1 < items.length) {
+        for lastCellsItemIndex: cellHeights.length
+        setMatrix(()=>getFoCno(fs, wiw).itemIndexMatrix);
+        for cellHeights.length: items.length
+        setMatrix(()=>getFoCno(fs, wiw).itemIndexMatrix);
+    }
+    getFoCno(fs, wiw).lastCellsItemIndex = this.items.push(item) - 1;
+    // itemIndexMatrix能改，我觉得是因为是shadow freeze的，这个得再试试。另外react应该不推荐这个写法。
+    this.layout.itemIndexMatrix[this.shortestColumnIndex].push(this.layout.lastCellsItemIndex);
+    this.setState(prevState => prevState);
 
     return (
-        <MasonryLayout matrix={}/>
+        <MasonryLayout matrix={matrix} getItem={getItem} setHight={setHeight}/>
     )
 };
 
