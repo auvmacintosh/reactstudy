4615c1f07d9abf5ed31578589cc9c5754d629bc9
auvmacintosh <auvmacintosh@qq.com>
2019-04-29 08:11:52 +0800

    try to understand functional component life method sequence

--- src/stage4_test/T.js
+++ src/stage4_test/T.js
@@ -2,38 +2,38 @@ import React, {useState, useEffect} from 'react';
 
 const T = () => {
     const [state, setState] = useState(0);
-    console.log(state);
-    if (state < 6) {
-        setState(prev => prev + 1);
-    }
-    console.log(state);
 
     useEffect(() => {
         console.log('T useEffect');
+        console.log(state);
+        if (state < 6) {
+            setState(prev => prev + 1);
+        }
         return () => {
             console.log('T useEffect return')
         }
-    }, []);
+    }, [state]);
     console.log('T render')
     return (
         <>
             T
             {state}
-            <A/>
+            <A state={state}/>
         </>
     )
 };
-const A = () => {
+const A = ({state}) => {
     useEffect(() => {
         console.log('A useEffect');
         return () => {
             console.log('A useEffect return')
         }
-    }, []);
+    }, [state]);
     console.log('A render')
     return (
         <>
             A
+            {state}
         </>
     )
 };
